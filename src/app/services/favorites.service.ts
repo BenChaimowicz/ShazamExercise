5b747e089f5fcce310decc5826d8e11c47c78ca0
Ben Chaimo <doroza@hotmail.com>
2019-05-04 19:38:45 +0300

    responsive

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,8 +1,8 @@
 import { Song } from './../song.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { JsonPipe } from '@angular/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,10 +14,23 @@ export class FavoritesService {
   constructor(private http: HttpClient) { }
 
   getAllFavorites(): Observable<Song[]> {
-    return this.http.get<Song[]>(this.baseURL + '/getAllFavorites');
+    return this.http.get<Song[]>(this.baseURL + '/getAllFavorites').pipe(
+      catchError(this.handleError));
   }
 
   addToFavorites(song: Song) {
-    return this.http.post<Song>(this.baseURL + '/addtofavorites', song);
+    return this.http.post<Song>(this.baseURL + '/addtofavorites', song).pipe(
+      catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    let errorMsg = '';
+    if (error.error instanceof ErrorEvent) {
+      errorMsg = 'Error ' + error.error.message;
+    } else {
+      errorMsg = 'Error ' + error.status + ' / ' + error.message;
+    }
+    return throwError(errorMsg);
   }
 }
+
